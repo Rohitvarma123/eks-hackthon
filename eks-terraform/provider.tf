2049082d60f2b7bdc07fbe1d086cd0759951b294
Rohitvarma123 <128877028+Rohitvarma123@users.noreply.github.com>
2026-03-05 18:52:07 +0530

    Update provider.tf

--- eks-terraform/provider.tf
+++ eks-terraform/provider.tf
@@ -1,11 +1,9 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
 terraform {
   required_version = ">= 1.5"
-
-  backend "s3" {
-    bucket = "devops-terraform-state-12345"
-    key    = "eks/terraform.tfstate"
-    region = "ap-south-1"
-  }
 
   required_providers {
     aws = {
