46f3bbe65ddcfff02ea04d7ead7bfd33aae3c6c7
Rohitvarma123 <128877028+Rohitvarma123@users.noreply.github.com>
2026-03-05 18:47:29 +0530

    1st commit

--- eks-terraform/provider.tf
+++ eks-terraform/provider.tf
@@ -1,9 +1,11 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
 terraform {
   required_version = ">= 1.5"
+
+  backend "s3" {
+    bucket = "devops-terraform-state-12345"
+    key    = "eks/terraform.tfstate"
+    region = "ap-south-1"
+  }
 
   required_providers {
     aws = {
@@ -11,4 +13,8 @@ terraform {
       version = "~> 5.0"
     }
   }
+}
+
+provider "aws" {
+  region = "ap-south-1"
 }
