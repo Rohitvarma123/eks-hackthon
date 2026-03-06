79d8ab0dee80a018df7089bfbf315822b55b322e
Rohitvarma123 <128877028+Rohitvarma123@users.noreply.github.com>
2026-03-06 09:37:59 +0530

    new commit

--- eks-terraform/eks.tf
+++ eks-terraform/eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.4"
 
-  cluster_name    = "devsecops-rohit"
+  cluster_name    = "devsecops-cluster"
   cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
